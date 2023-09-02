500cd55ed7dca4a55a6acb283e00d9c53c415656
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-09-02 12:33:54 -0300

    add GPS

--- coordinates.py
+++ coordinates.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import config
 
 
 @dataclass(slots=True, frozen=True)
@@ -9,4 +10,11 @@ class Coordinates:
 
 def get_gps_coordinates() -> Coordinates:
     """Returns current coordinates using GPS"""
-    return Coordinates(-34.58126316837909, -58.433659373668306)
+    latitude, longitude = (-34.58126316837909, -58.433659373668306)
+    if config.USE_ROUNDED_COORDS:
+        latitude, longitude = map(lambda c: round(c, 4), [latitude, longitude])
+    return Coordinates(latitude, longitude)
+
+
+if __name__ == "__main__":
+    print(get_gps_coordinates())
