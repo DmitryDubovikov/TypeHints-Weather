e1d53d09acff9535be7ed3ec58c2ba6b558d4fc6
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-09-02 13:35:44 -0300

    add get_gps_coordinates

--- coordinates.py
+++ coordinates.py
@@ -1,19 +1,40 @@
 from dataclasses import dataclass
+from subprocess import run, CompletedProcess
+
 import config
+from exceptions import CantGetCoordinates
 
 
 @dataclass(slots=True, frozen=True)
 class Coordinates:
-    longitude: float
     latitude: float
+    longitude: float
 
 
 def get_gps_coordinates() -> Coordinates:
-    """Returns current coordinates using GPS"""
-    latitude, longitude = (-34.58126316837909, -58.433659373668306)
-    if config.USE_ROUNDED_COORDS:
-        latitude, longitude = map(lambda c: round(c, 4), [latitude, longitude])
-    return Coordinates(latitude, longitude)
+    """Returns current coordinates using ipinfo"""
+    coordinates = _get_ipinfo_coordinates()
+    return _round_coordinates(coordinates)
+
+
+def _get_ipinfo_coordinates() -> Coordinates:
+    result: CompletedProcess[str] = run(
+        ["curl", "ipinfo.io/loc"], capture_output=True, text=True
+    )
+    if result.returncode != 0:
+        raise CantGetCoordinates
+    return _parse_coordinates(result.stdout.strip())
+
+
+def _parse_coordinates(ipinfo_result: str) -> Coordinates:
+    coords = ipinfo_result.split(",")
+    return Coordinates(latitude=float(coords[0]), longitude=float(coords[1]))
+
+
+def _round_coordinates(coords: Coordinates) -> Coordinates:
+    if not config.USE_ROUNDED_COORDS:
+        return coords
+    return Coordinates(round(coords.latitude, 4), round(coords.longitude, 4))
 
 
 if __name__ == "__main__":
